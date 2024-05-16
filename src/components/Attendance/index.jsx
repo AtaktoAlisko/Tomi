91a69634e51687e396f1b7c2dcd4337dce968a41
Алибек Мурат <alibekmurat@MacBook-Pro-Alibek.local>
2024-05-17 00:04:26 +0500

    second

---
+++ src/components/Attendance/index.jsx
@@ -0,0 +1,137 @@
+"use client";
+import { useState } from "react";
+import styles from "./attendance.module.scss";
+
+export default function Attendance() {
+  const [attendance, setAttendance] = useState("");
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  function Submit(e) {
+    e.preventDefault();
+    const formEle = document.querySelector("form");
+    const formDatab = new FormData(formEle);
+
+    formDatab.append("attendance", attendance);
+
+    setLoading(true);
+    fetch(
+      "https://script.google.com/macros/s/AKfycbyp-VVf7afBotIsgC1eyVI0W7mRFyMcV9CtAetHuD6tHnOegquXUoCv-Gojfx8eHMo/exec",
+      {
+        method: "POST",
+        body: formDatab,
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setAttendance("");
+        setTitle("");
+        setText("");
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }
+
+  if (isSubmitted) {
+    return (
+      <div className={styles.App}>
+        <p className="text-center min-w-[300px] text-[18px] sm:text-[20px] mb-[10px]">
+          Cіздің жауабыңыз қабылданды!
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.App}>
+      <div className="form center mb-20 mt-10">
+        <p
+          className={`${styles.anketa } , text-center min-w-[300px] text-[22px] sm:text-[22px] mb-[10px] `}
+        >
+          Анкетаны толтыруыңызды сұраймыз
+        </p>
+
+        <p
+          className={`${styles.anketa} text-center min-w-[300px] text-[22px] sm:text-[22px] mb-[10px]`}
+        >
+          Біз үшін өте маңызды!
+        </p>
+        <div className="min-w-[300px]: mx-[20px] sm: mx-auto relative mt-6">
+          <form className="form" onSubmit={(e) => Submit(e)}>
+            <div className="min-w-[300px]: mx-5">
+              <input
+                className="min-w-[300px]:text-[14px] sm:text-[20px]"
+                placeholder="Атыңыз"
+                name="Name"
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+              <textarea
+                className="h-[100px] min-w-[300px]:text-[14px] sm:text-[20px] w-full p-2 rounded-lg outline-none"
+                placeholder="Тілектеріңіз"
+                name="Message"
+                type="text"
+                onChange={(e) => setText(e.target.value)}
+                value={text}
+              />
+            </div>
+            <div className={styles.radio}>
+              <div className="flex items-center ml-2">
+                <input
+                  type="radio"
+                  id="willAttend"
+                  value="Келемін"
+                  checked={attendance === "Келемін"}
+                  onChange={() => setAttendance("Келемін")}
+                />
+                <label htmlFor="willAttend" className="ml-2">
+                  Келемін
+                </label>
+              </div>
+              <div className="flex items-center  ml-2">
+                <input
+                  type="radio"
+                  id="willAttendWithWife"
+                  value="Жұбайымен"
+                  checked={attendance === "Жұбайымен"}
+                  onChange={() => setAttendance("Жұбайымен")}
+                />
+                <label htmlFor="willAttendWithWife" className="ml-2">
+                  Жұбайымен
+                </label>
+              </div>
+              <div className="flex items-center ml-2">
+                <input
+                  type="radio"
+                  id="cannotAttend"
+                  value="Келе алмаймын"
+                  checked={attendance === "Келе алмаймын"}
+                  onChange={() => setAttendance("Келе алмаймын")}
+                />
+                <label htmlFor="cannotAttend" className="ml-2">
+                  Келе алмаймын
+                </label>
+              </div>
+            </div>
+            <div className="flex-1 flex justify-center min-w-[300px]: mx-5">
+              <button
+                className={styles.button}
+                type="submit"
+                disabled={isLoading}
+              >
+                Жіберу
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Main/index.jsx
@@ -0,0 +1,52 @@
+"use client";
+
+import Image from "next/image";
+import Tomiris from "../../../public/images/Tomiris.png";
+import { useState } from "react";
+import { useEffect } from "react";
+import muz from "../../assets/muzik.mp3";
+import styles from "./main.module.scss";
+import proigr from "../../../public/images/proigr.png";
+
+export default function Main() {
+  const [audio, setAudio] = useState(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    setAudio(new Audio(muz));
+  }, []);
+
+  function play() {
+    if (audio) {
+      if (isPlaying) {
+        audio.pause();
+      } else {
+        audio.play();
+      }
+      setIsPlaying(!isPlaying);
+    }
+  }
+
+  return (
+    <div className="flex flex-col items-center mt-20 mb-20 ">
+       <div className="absolute sm:left-10 sm:top-10 min-w-[300px]: left-5 top-20">
+        <button
+          className={`${styles.muzik} ${isPlaying ? "animate-spin" : ""} `}
+          onClick={play}
+        >
+          <Image height={35} width={80} src={proigr} alt="musik" />
+        </button>
+      </div>
+      <p className="shimmer-text text-[30px] sm:text-[30px]">Томирис</p>
+      <Image
+        className="mr-20 mb-20"
+        src={Tomiris}
+        alt="Tomiris"
+        width={300}
+        height={300}
+      />
+      <p className="shimmer-text text-[30px] sm:text-[30px] mb-5">1 Жас</p>
+      <p className=" shimmer-text text-[30px] sm:text-[30px]">Түсау кесер</p>
+    </div>
+  );
+}
